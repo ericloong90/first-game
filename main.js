8fcfaa4aad46572e14493ecbf626a8a36b79f7cd
Eric Tai Chi Loong <ericloong90@gmail.com>
2016-04-06 18:49:54 +0800

    added coin

--- main.js
+++ main.js
@@ -11,6 +11,7 @@ var mainState = {
 		game.load.image('player', 'assets/player.png');
 		game.load.image('wallV', 'assets/wallVertical.png');
 		game.load.image('wallH', 'assets/wallHorizontal.png');
+		game.load.image('coin', 'assets/coin.png');
 	},
 
 	create: function () {
@@ -31,6 +32,68 @@ var mainState = {
 
 		// This line adds the keyboard arrow functionality to the game
 		this.cursor = game.input.keyboard.createCursorKeys();
+
+		this.createWorld();
+
+		// Display the coin
+		this.coin = game.add.sprite(60, 140, 'coin');
+		// Add arcade physics to the coin
+		game.physics.arcade.enable(this.coin);
+		// Set the anchor point of the coin to its center
+		this.coin.anchor.setTo(0.5, 0.5);
+
+		// Display the score
+		this.scoreLabel = game.add.text(30, 30, 'score: 0', { font: '18px Arial', fill: '#ffffff'});
+		// Initialize the score variable
+		this.score = 0;
+
+	},
+
+	update: function () {
+		// This function is called 60 times per second
+		// It contains the game's logic
+
+		// Add collision physics to the game
+		game.physics.arcade.collide(this.player, this.walls);
+
+		this.movePlayer();
+
+		game.physics.arcade.overlap(this.player, this.coin, this.takeCoin, null, this);
+
+		if (!this.player.inWorld) {
+			this.playerDie();
+		}
+	
+	},
+
+	// And here we will later add some of our own functions
+
+	// This function is used to move player in the game
+	movePlayer: function () {
+		// if the left arrow key is pressed
+		if (this.cursor.left.isDown) {
+			// Move player to the left
+			this.player.body.velocity.x = -200;
+		}
+
+		// if the right arrow key is pressed
+		else if (this.cursor.right.isDown) {
+			// Move player to the right
+			this.player.body.velocity.x = 200;
+		}
+
+		else {
+			this.player.body.velocity.x = 0;
+		}
+
+		// if the up arrow key is pressed and the player is touching the ground
+		if (this.cursor.up.isDown && this.player.body.touching.down) {
+			// Move the player upward/jump
+			this.player.body.velocity.y = -320;
+		}
+	},
+
+	createWorld: function() {
 
 		// Create a new group
 		this.walls = game.add.group();
@@ -58,46 +121,33 @@ var mainState = {
 
 		// Set all the walls to be immovable
 		this.walls.setAll('body.immovable', true);
-
-
-	},
-
-	update: function () {
-		// This function is called 60 times per second
-		// It contains the game's logic
-
-		this.movePlayer();
-	},
-
-	// And here we will later add some of our own functions
-
-	// This function is used to move player in the game
-	movePlayer: function () {
-		// if the left arrow key is pressed
-		if (this.cursor.left.isDown) {
-			// Move player to the left
-			this.player.body.velocity.x = -200;
-		}
-
-		// if the right arrow key is pressed
-		else if (this.cursor.right.isDown) {
-			// Move player to the right
-			this.player.body.velocity.x = 200;
-		}
-
-		else {
-			this.player.body.velocity.x = 0;
-		}
-
-		// if the up arrow key is pressed and the player is touching the ground
-		if (this.cursor.up.isDown && this.player.body.touching.down) {
-			// Move the player upward/jump
-			his.player.boy.velocity.y = -320;
-		}
-	},
-
-	createWorld: function() {
 		
+	},
+
+	playerDie: function () {
+		game.state.start('main');
+	},
+
+	takeCoin: function (player, coin) {
+		// Define 2 random variables
+		var newX = game.rnd.integerInRange(10, game.world.width - 20);
+		var newY = game.rnd.integerInRange(10, game.world.height - 20);
+
+		// Set the new coin position
+		this.coin.reset(newX, newY);
+
+		// Increase the score by 5
+		this.score += 5;
+
+		// Update the score label
+		this.scoreLabel.text = 'score: ' + this.score;
+	},
+
+	updateCoinPosition: function () {
+		// Store all the possible coin positions in an array
+		var coinPosition = [{x: 140, y: 60}, {x: 360, y: 60},	// Top row
+												{x: 60, y: 140}, {x: 440, y: 140},	// 2nd row
+												{x: 130, y: 300}, {x: 370, y: 300}];	// 3rd row
 	}
 };
 
